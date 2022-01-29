a6068047eb63fc1b46b11c8ec4591554c2a2aed9
Adithya Ajith <4adhi007@gmail.com>
2022-01-29 13:21:34 +0530

    UI edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,17 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav style={{backgroundColor: 'red' }} className="navbar  fixed-top  flex-md-nowrap p-0 shadow">
+      <nav style={{backgroundColor: '#FFD700' }} className="navbar  fixed-top  flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
          
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" /> */}
-          <p style={{color: 'black'}}>Cryptogram </p>
-          place to smart your contracts..
+          { <img src="cryptologo.png" width="85" height="85" className="d-inline-block align-top" alt="" /> }
+           
         </a>
+        <h4 style={{ position: 'absolute', top:'10px', left:'10%', fontFamily: 'fantasy', fontSize:'20px' } } >Place to smart your contracts</h4>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
